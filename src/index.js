20de6ce5ca1e8354ab52ad7efc763ff436fa95e6
Ilja <tadden12@gmail.com>
2021-09-21 16:18:32 +0300

    final

--- src/index.js
+++ src/index.js
@@ -4,40 +4,50 @@ import getRefs from './js/get-refs';
 import countryCard from './templates/country-card.hbs';
 import countryList from './templates/country-list.hbs';
 
-
-
-
 import debounce from 'lodash.debounce';
+
 import"@pnotify/core/dist/PNotify.css";
 import"@pnotify/core/dist/BrightTheme.css";
-import { error, info, defaultStack, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import { error, alert, defaultStack, defaultModules } from '@pnotify/core/dist/PNotify.js';
 import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/mobile/dist/PNotifyMobile.css';
-import { delay } from 'lodash';
+
 
 defaultModules.set(PNotifyMobile, {});
 
 const refs = getRefs();
 
 
-refs.searchForm.addEventListener('input', debounce(onInputChange, 500))
+
+refs.searchForm.addEventListener('input', debounce(onSearch, 500))
 
 
-function renderCountryCard(country) {
-    const markupCard = countryCard(country)
-    refs.cardContainer.insertAdjacentHTML('afterbegin', markupCard);
-}
-
-function renderCountryList (country) {
-    const markupList = countryList(country);
-    refs.cardContainer.insertAdjacentHTML('afterbegin', markupList)
-}
-
-
-function onInputChange(e) {
+function onSearch(e) {
     const searchQuery = e.target.value;
-    API.getfetchCountries(searchQuery).then(renderCountryCard)
+    API.getfetchCountries(searchQuery).then(rendering).catch(onFetchError)
 }
 
+function rendering(country) {
+    if (country.status === 404) {
+        refs.cardContainer.innerHTML = '';
+        alert({ text: 'Check the correctness of the data entered, this country does not exist!' })
+        return
+    }
+    else if (country.length > 10) {
+        refs.cardContainer.innerHTML = ''
+        error({ text: 'Too many matches found. Please enter a more specific query!' })
+        return
+    }
+        
+    else if (country.length > 1) {
+        refs.cardContainer.innerHTML = countryList(country)
+        return
+    }
+    refs.cardContainer.innerHTML = countryCard(country)
+}
 
+function onFetchError(err) {
+  refs.cardContainer.innerHTML = ''
+  alert({ text: 'Check the correctness of the data entered!' })
+}
