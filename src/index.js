56f8cb24215bf48e170a4393f9e820503e6326fd
Ilja <tadden12@gmail.com>
2021-09-21 14:57:17 +0300

    step 2

--- src/index.js
+++ src/index.js
@@ -3,8 +3,24 @@ import API from './js/fetchCountries';
 import getRefs from './js/get-refs';
 import countryCard from './templates/country-card.hbs';
 import countryList from './templates/country-list.hbs';
+import debounce from 'lodash.debounce';
+
+
+import debounce from 'lodash.debounce';
+import"@pnotify/core/dist/PNotify.css";
+import"@pnotify/core/dist/BrightTheme.css";
+import { error, info, defaultStack, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
+import { delay } from 'lodash';
+
+defaultModules.set(PNotifyMobile, {});
 
 const refs = getRefs();
+
+
+refs.searchForm.addEventListener('input', debounce(onInputChange, 500))
 
 
 function renderCountryCard(country) {
@@ -18,12 +34,8 @@ function renderCountryList (country) {
 }
 
 
-refs.searchForm.addEventListener('input', onInputChange)
-
-
 function onInputChange(e) {
     const searchQuery = e.target.value;
-
     API.getfetchCountries(searchQuery).then(renderCountryCard)
 }
 
